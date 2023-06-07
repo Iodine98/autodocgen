2359e3f98ec1c33caca0bcc8489c38d5bfb4f8a7
Rohan Sobha <r.r.sobha@student.tudelft.nl>
2023-06-07 20:15:32 +0200

    Documented file visitor.

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import logging
 import time
 
-from src.autodocgen.method_visitor import FileVisitor
+from src.autodocgen.file_visitor import FileVisitor
 from src.autodocgen.ast_analyzer import ASTAnalyzer
 
 logging.getLogger().setLevel(logging.INFO)
@@ -10,10 +10,10 @@ logging.getLogger().setLevel(logging.INFO)
 def main():
     start_time = time.time()
     ast_analyzer = ASTAnalyzer()
-    ast_analyzer.load_ast_from_file('./src/autodocgen/ast_analyzer.py')
+    ast_analyzer.load_ast_from_file('./src/autodocgen/file_visitor.py')
     method_visitor = FileVisitor(ast_analyzer)
     ast_analyzer.generate_documentation(method_visitor)
-    ast_analyzer.write_file_from_ast('./ast_analyzer_documented.py')
+    ast_analyzer.write_file_from_ast('./file_visitor_doc.py')
     end_time = time.time()
     logging.info("Executed in %ds", end_time - start_time)
 
