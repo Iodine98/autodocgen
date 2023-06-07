d43a7f8a9d64179c7f761ff9dbb6c732afe18a8b
Rohan Sobha <r.r.sobha@student.tudelft.nl>
2023-06-07 19:00:12 +0200

    Method visitor is working.

--- src/autodocgen/ast_analyzer.py
+++ src/autodocgen/ast_analyzer.py
@@ -1,45 +1,103 @@
 import ast
-import astor
-import re
-from pathlib import Path
-import astunparse
-from _ast import FunctionDef, AST
-from typing import Tuple, Union, Optional, List, Any
-import openai
-import dotenv
 import os
+import re
+from _ast import FunctionDef, AST
+from pathlib import Path
+from typing import Tuple, Union, Optional, Literal, TypedDict
 
-from src.autodocgen.method_visitor import MethodVisitor
+import black
+import dotenv
+import openai
+
+dotenv.load_dotenv()
+
+
+class ChatMessage(TypedDict):
+    """
+    ChatMessage class for type hints in OpenAI
+    """
+    role: Union[Literal["user"], Literal["system"], Literal["assistant"]]
+    content: str
+
+
+class ModelKwargs(TypedDict):
+    """
+    ModelKwargs class for type hints in OpenAI
+    """
+    max_tokens: int
+    model: str
+    messages: list[ChatMessage]
+    n: Optional[int]
+    stop: Optional[Union[str, list]]
+    temperature: int
 
 
 class ASTAnalyzer:
+    default_messages: list[ChatMessage] = [
+        ChatMessage(
+            role="user",
+            content="Act as a Python software engineer that needs to write PyDoc for their "
+                    "modules, classes and methods.\n"
+                    "The PyDoc should be formatted as specified in PEP 257.\n"
+                    "The line length of 100 characters should not be exceeded if possible.\n"
+                    "The example will follow."
+        )
+    ]
+    default_model_kwargs = ModelKwargs(
+        max_tokens=1024,
+        model="gpt-3.5-turbo",
+        messages=default_messages,
+        n=1,
+        stop=None,
+        temperature=0
+    )
+    default_prepend_prompt = 'Write Pydoc for the function below and only return the PyDoc:\n\n'
 
-    def __init__(self, model_name='text-davinci-003', model_kwargs: dict = None,
-                 prepend_prompt: str = 'Write Pydoc for the function below and only return the '
-                                       'PyDoc:\n\n',
-                 **kwargs):
+    def __init__(self, model_kwargs: ModelKwargs = None,
+                 prepend_prompt: str = default_prepend_prompt, **kwargs):
+        self.messages = self.default_messages
+        self.model_kwargs: ModelKwargs = model_kwargs or self.default_model_kwargs
+        self.prepend_prompt = prepend_prompt
+
         self.source_code: Optional[str] = None
         self.tree: Optional[AST] = None
-        dotenv.load_dotenv()
-        openai.api_key = os.getenv('OPENAI_KEY', None)
+        self.latest_response = None
+        self.total_token_usage = 0
+
+        openai.api_key = os.getenv("OPENAI_KEY", None)
         if openai.api_key is None:
-            raise EnvironmentError('OPENAI_KEY is missing')
-        self.prepend_prompt = prepend_prompt
-        self.model_kwargs = ({'engine': model_name, 'max_tokens': 1024, 'n': 1, 'stop': None,
-                              'temperature': 0} | ({} if (model_kwargs is None) else model_kwargs))
+            raise EnvironmentError("OPENAI_KEY is missing")
         if 'file_path' in kwargs:
             self.load_ast_from_file(kwargs['file_path'])
+        if 'line_length' in kwargs:
+            self.line_length = kwargs["line_length"]
+        else:
+            self.line_length = 100
 
     def load_ast_from_file(self, file_path: Union[(str, Path)]):
-        with open(file_path) as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             self.source_code = file.read()
         self.tree: AST = ast.parse(self.source_code)
 
-    def obtain_pydoc(self, text) -> Tuple[str, int]:
-        response = openai.Completion.create(
-            prompt=(self.prepend_prompt + text),
-            **self.model_kwargs)
-        return response.choices[0].text.strip(), response.usage['total_tokens']
+    def trigger_chat_completion(self) -> None:
+        self.latest_response = openai.ChatCompletion.create(**self.model_kwargs)
+
+    def obtain_pydoc(self, text: str = None) -> str:
+        if text:
+            self.model_kwargs['messages'].append(
+                ChatMessage(
+                    role='user',
+                    content=self.prepend_prompt + text,
+                )
+            )
+        self.trigger_chat_completion()
+        latest_message = self.latest_response.choices[0].message
+        self.messages.append(ChatMessage(
+            role=latest_message.role,
+            content=latest_message.content
+        ))
+        self.total_token_usage += self.latest_response.usage['total_tokens']
+        return latest_message.content.strip()
 
     @staticmethod
     def get_signature(current_method: FunctionDef) -> str:
@@ -50,17 +108,31 @@ class ASTAnalyzer:
         return str(tuple((method_name, method_arguments, method_return_type)))
 
     def write_file_from_ast(self, file_path: Union[(str, Path)], str_return=False) -> Optional[str]:
-        new_code: str = astunparse.unparse(tree=self.tree)
-        with open(file_path, 'w') as file:
+        new_code: str = ast.unparse(ast_obj=self.tree)
+        new_code: str = black.format_str(new_code,
+                                         mode=black.Mode(line_length=self.line_length))
+
+        with open(file_path, 'w', encoding='utf-8') as file:
             file.write(new_code)
         if str_return:
             return new_code
+        return None
 
-    @staticmethod
-    def add_docstring_to_ast(node, docstring):
-        clean_docstring = docstring.replace("\"\"\"", "")
+    def add_docstring_to_ast(self, node: FunctionDef, new_docstring: str):
+        existing_docstring = ast.get_docstring(node)
+        regex_match = re.search(r"\"\"\"([A-Za-z0-9-_\s(),:=*.'->]+)\"\"\"", new_docstring)
+        alt_match = re.search(r"```([A-Za-z0-9-_\s(),:=*.'->\"]+)```", new_docstring)
+        if regex_match:
+            clean_docstring = regex_match.group(1)
+        elif alt_match:
+            clean_docstring = alt_match.group(1)
+        else:
+            clean_docstring = "The docstring could not be extracted."
         docstring_node = ast.Expr(value=ast.Str(s=clean_docstring))
-        node.body.insert(0, docstring_node)
+        if existing_docstring:
+            node.body[0] = docstring_node
+        else:
+            node.body.insert(0, docstring_node)
 
-    def generate_documentation(self, method_visitor: 'MethodVisitor'):
+    def generate_documentation(self, method_visitor):
         method_visitor.visit(self.tree)
--- src/autodocgen/method_visitor.py
+++ src/autodocgen/method_visitor.py
@@ -1,18 +1,38 @@
 import ast
+import logging
+import random
+import time
+
 import astor
 from _ast import FunctionDef
 
-from src.autodocgen import ASTAnalyzer
+from openai.error import RateLimitError
+
+from src.autodocgen.ast_analyzer import ASTAnalyzer
 
 
-class MethodVisitor(ast.NodeVisitor):
+class MethodVisitor(ast.NodeTransformer):
 
     def __init__(self, ast_analyzer: ASTAnalyzer):
         self.ast_analyzer = ast_analyzer
+        self.stop_loop = False
 
-    def visit_FunctionDef(self, node: FunctionDef) -> None:
-        function_code = astor.to_source(node)
-        response, _ = self.ast_analyzer.obtain_pydoc(function_code)
-        self.ast_analyzer.add_docstring_to_ast(node, docstring=response)
+    def obtain_pydoc_wrapper(self, node: FunctionDef, source_code: str) -> str:
+        try:
+            return self.ast_analyzer.obtain_pydoc(source_code)
+        except RateLimitError:
+            time.sleep(random.randint(5, 10))
+            return self.obtain_pydoc_wrapper(node, source_code)
 
+    def visit_FunctionDef(self, node: FunctionDef) -> FunctionDef:
+        # if self.stop_loop:
+        #     return node
+        # self.stop_loop = True
+        time.sleep(random.randint(2, 5))
+        logging.info("Method name: %s", node.name)
+        source_code = astor.to_source(node)
+        response = self.obtain_pydoc_wrapper(node, source_code)
+        logging.info(response)
+        self.ast_analyzer.add_docstring_to_ast(node, new_docstring=response)
+        return node
 
